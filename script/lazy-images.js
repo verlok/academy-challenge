7ff06431adb02a67bbc9856ea9b2bad5b534f052
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-04-06 11:54:41 +0200

    Eagerly loaded in-viewport images

--- script/lazy-images.js
+++ script/lazy-images.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll("img");
+const images = document.querySelectorAll("img.lazy");
 
 const lazyImageObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
